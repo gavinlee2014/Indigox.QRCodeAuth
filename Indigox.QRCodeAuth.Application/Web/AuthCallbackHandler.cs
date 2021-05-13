5d881c1df99a185fa0a5d2044035d36c4a879313
Gavin Lee <flashcode2000@qq.com>
2021-05-13 18:00:15 +0800

    成功时直接显示页面

--- Indigox.QRCodeAuth.Application/Web/AuthCallbackHandler.cs
+++ Indigox.QRCodeAuth.Application/Web/AuthCallbackHandler.cs
@@ -18,6 +18,49 @@ namespace Indigox.QRCodeAuth.Application.Web
     {
         public bool IsReusable => true;
 
+        private readonly string successResponse = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8' />
+    <title>扫码成功</title>
+
+    <style type='text/css'>
+        body {
+            font-size: 14px;
+            font-family: PingFang-SC-Regular,Helvetica,sans-serif;
+        }
+        .content {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            width: 100%;
+            text-align: center;
+        }
+        .content h1
+        {
+            font-size: 10em;
+            color: #f4ad42;
+        }
+        .content p
+        {
+            font-size: 4em;
+            color: #333;
+        }
+        .count_down
+        {
+            color: #f4ad42;
+        }
+    </style>
+</head>
+<body>
+    <div class='content'>
+         <h1>扫码成功</h1>
+         <p>请关闭当前页以继续</p>
+     </div>
+ </body>
+ </html>";
+
 
         public void ProcessRequest(HttpContext context)
         {
@@ -29,7 +72,8 @@ namespace Indigox.QRCodeAuth.Application.Web
             Log.Debug("id " + id + " get user name " + userName);
 
             _ = MessageSender.Instance.SendAsync("TRANS", id + " " + userName);
-            context.Response.Write("<html><header><title></title><script type='text/javascript'>window.location.href='success.html'</script></header><body></body></html>");
+            context.Response.Write(successResponse);
+            //context.Response.Write("<html><header><title></title><script type='text/javascript'>window.location.href='success.html'</script></header><body></body></html>");
         }
 
     }
